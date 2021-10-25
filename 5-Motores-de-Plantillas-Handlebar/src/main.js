3aa7fb9ef10a1adfd5cef5ab09cb123a64d1fb2a
Lucas Cotton <lcotton@itba.edu.ar>
2021-10-25 16:02:38 +0300

    handlebars

---
+++ 5-Motores-de-Plantillas-Handlebar/src/main.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+
+const productos = []
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+
+app.engine('hbs', exphbs({
+    extname: 'hbs',
+    defaultLayout: 'inicio.hbs'
+  }))
+
+app.set('view engine', 'hbs')
+app.set('views', './views')
+
+app.get('/productos', (req,res) => {
+  res.render('formulario',{layout: 'inicio'})
+})
+
+app.post('/productos', (req, res) => {
+  productos.push(req.body)
+  res.render('historial', {productos,hayProductos:productos.length>0});
+});
+
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+})
+server.on('error', error => console.log(`Error en servidor ${error}`))
