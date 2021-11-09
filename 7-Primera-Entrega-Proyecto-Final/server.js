197b55a2d9c6756a6e93537faad5e127a7d1a3f5
Lucas Cotton <lcotton@itba.edu.ar>
2021-11-09 10:55:41 +0200

    Primera Entrega Proyecto Final

--- 7-Primera-Entrega-Proyecto-Final/server.js
+++ 7-Primera-Entrega-Proyecto-Final/server.js
@@ -1,15 +1,29 @@
-import express from 'express'
-const app = express()
-import { routerProductos } from "./router/routerProductos.js"
+import express from "express";
+const app = express();
+import { routerProductos } from "./router/routerProductos.js";
+import { routerCarritos } from "./router/routerCarritos.js";
 
+app.use(express.json());
 
-app.use(express.json())
+app.use("/api/productos", routerProductos);
+app.use("/api/carrito", routerCarritos);
 
-app.use('/api/productos', routerProductos)
+app.get("*", (req, res) => {
+  next(error);
+});
 
-const PORT = 8080
+app.use(function (err, req, res, next) {
+  res
+    .status(500)
+    .json({
+      error: -2,
+      descripcion: `ruta ${req.originalUrl} método ${req.method} no implementada`,
+    });
+});
+
+const PORT = process.env.port || 8080;
 
 const server = app.listen(PORT, () => {
-    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
-})
-server.on("error", error => console.log(`Error en servidor ${error}`))
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+});
+server.on("error", (error) => console.log(`Error en servidor ${error}`));
